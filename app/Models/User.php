c81b45019061aa7572518b96d75c3c9178dfef0f
ENCODE <Kaprielov93@gmail.com>
2026-04-10 15:10:06 +0300

    Disable timestamps on User model (WebUser has no updated_at)

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,6 +15,7 @@ class User extends Authenticatable implements FilamentUser, HasName
     use HasFactory, Notifiable;
 
     protected $table = 'WebUser';
+    public $timestamps = false;
 
     protected $fillable = [
         'firstName',
